a551a5591506eb0a52df8e3291a158e5436da711
Axujen <axujen@gmail.com>
2013-06-07 18:19:08 +0100

    Reply to even small messages

--- src/gentoobot/brain.py
+++ src/gentoobot/brain.py
@@ -33,32 +33,37 @@ class Brain(object):
 
 	def process_line(self, line):
 		words = re.sub(r'[^a-zA-Z\s]', '', line).split()
-		for id, word in zip(xrange(len(words)-2), words):
-			next = words[id+1]
-			val1 = words[id+2]
+		if len(words) >= 3:
+			for id, word in zip(xrange(len(words)-2), words):
+				next = words[id+1]
+				val1 = words[id+2]
 
-			key = ' '.join((word, next)).lower()
-			self.db.sadd(key, val1)
+				key = ' '.join((word, next)).lower()
+				self.db.sadd(key, val1)
 		return words
 
 	def generate_sentence(self, msg):
 		"""Generate a sentence based on msg"""
 		logger.warning('Generating sentence based on %s', msg)
 		words = self.process_line(msg)
-		seed = random.randint(0, len(words)-2)
-		seed, next, = words[seed], words[seed+1]
-		w1, w2 = seed, next
-		sentence, w = [w1, w2], 2
-		for i in xrange(random.randint(len(words)-5, len(words)+5)):
+		if len(words) >= 2:
+			seed = random.randint(0, len(words)-2)
+			seed = ' '.join((words[seed], words[seed+1])).lower()
+		else:
+			seed = self.db.randomkey()
+
+		return self._generate_from_seed(seed)
+
+	def _generate_from_seed(self, seed):
+		"""Generate a message seeded by a list of words"""
+		w1, w2 = seed.split()
+		sentence = [w1, w2]
+		for i in xrange(random.randint(3, 20)):
 			key = ' '.join((w1, w2)).lower()
 			if self.db.exists(key):
 				w3 = self.db.srandmember(key)
 			else:
-				try:
-					w3 = words[w]
-				except IndexError:
-					return ' '.join(sentence)
+				w3 = self.db.srandmember(self.db.randomkey())
 			sentence.append(w3)
 			w1, w2 = w2, w3
-			w += 1
 		return ' '.join(sentence)
