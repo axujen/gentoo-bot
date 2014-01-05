a5e4701d83e76748acc098e568f816319bc11509
Axujen <axujen@autistici.org>
2014-01-05 19:24:17 +0000

    Refactor take 2
    Remove reply methods pending rework
    Remove commands module pending rework
    Remove who methods as they were badly implemented
    Import whole modules instead of just functions
    Add _ghost() method

--- src/gentoobot/core.py
+++ src/gentoobot/core.py
@@ -14,21 +14,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.	If not, see <http://www.gnu.org/licenses/>.
 
-import re, random, json
+import re, random, json, time, thread, signal
 from urllib2 import urlopen, Request
 from urlparse import urlparse
-from time import time, sleep
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from thread import start_new_thread
-from signal import signal, SIGINT
 
-from bs4 import BeautifulSoup
 import irc.bot
 from irc.client import NickMask, Event
+from bs4 import BeautifulSoup
 
 from gentoobot import logger
 from gentoobot.config import get_config, load_db, config_base
-from gentoobot.commands import commands
 
 # Replace characters when unable to decode them properly
 import codecs
@@ -46,7 +42,6 @@ class GentooBot(irc.bot.SingleServerIRCBot):
 
 		self.reconnect = reconnect
 		self.chans = [channel]
-		self.chattiness = options['chattiness']
 		self.admins = load_db(server, "admins")
 
 		# This is the bots original nick, used for ghosting.
@@ -55,14 +50,6 @@ class GentooBot(irc.bot.SingleServerIRCBot):
 		self.server = server
 		self.port = int(port)
 		self.password = password
-
-		self.wholist = {}
-		self.replies = self._get_replies()
-		self.timed_replies = self._get_timed_replies()
-		self.last_reply = 0
-
-		self.url_pattern = re.compile(r"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
-		self.smiley_pattern = re.compile(r"([:x=;]{1,2}(?:\)|D|P|O|o))(?:\s|$)", re.I)
 
 	def on_welcome(self, c, e):
 		logger.log_event(self.server, e)
@@ -78,25 +65,15 @@ class GentooBot(irc.bot.SingleServerIRCBot):
 		channel = e.target
 		user = e.source
 		message = e.arguments[0]
-		self.pub_commands(channel, user, message)
 
 	def on_privmsg(self, c, e):
 		logger.log_event(self.server, e)
 		user = e.source
 		msg = e.arguments[0]
-		self.priv_commands(user, msg)
-
-	def pub_commands(self, channel, user, message):
-		start_new_thread(commands.run, (self, channel, user, message))
-		self.reply(channel, user, message)
-
-	def priv_commands(self, user, message):
-		"""What to do when receiving commands in privmsg"""
-		start_new_thread(commands.run, (self, user.nick, user, message))
 
 	def on_kick(self, c, e):
 		logger.log_event(self.server, e)
-		sleep(self.reconnect)
+		time.sleep(self.reconnect)
 		self.join(e.target)
 
 	def join(self, channel):
@@ -113,44 +90,12 @@ class GentooBot(irc.bot.SingleServerIRCBot):
 		if channel in self.chans:
 			self.chans.remove(channel)
 
-	def on_whoreply(self, c, e):
-		args = e.arguments
-		fields = ('channel', 'realname', 'host', 'server', 'nick', 'flags', 'tail')
-		who = {}
-
-		for item, field in zip(args, fields):
-			if field == 'tail':
-				continue
-			who[field] = item
-
-		self.wholist[who['nick'].lower()] = who
-		self.whostatus = 'ACK'
-
 	def on_nicknameinuse(self, c, e):
 		self.nick += '_'
 		c.nick(self.nick)
 
 		if self.password:
-			self.connection.privmsg('nickserv', 'ghost %s %s' % (self.my_nick, self.password))
-			self.connection.nick(self.my_nick)
-			self.nick = self.my_nick[:]
-			self._identify()
-
-	def who(self, nick, timeout=5):
-		if isinstance(nick, NickMask):
-			nick = nick.nick
-
-		self.whostatus = 'REQUEST'
-		self.connection.who(nick)
-
-		n = 0
-		while not self.whostatus == 'ACK':
-			if n >= timeout:
-				return "REQUEST TIMEOUT"
-			n += 1
-			sleep(1)
-
-		return self.wholist[nick.lower()]
+			self._ghost(self.password)
 
 	def say(self, channel, message):
 		if isinstance(message, str):
@@ -176,91 +121,19 @@ class GentooBot(irc.bot.SingleServerIRCBot):
 
 		self.say(channel, "%s, %s" % (user, message))
 
-	def reply(self, channel, user, message):
-		"""Reply to someone"""
-		for reply in self.replies:
-			status = getattr(self, reply)(channel, user, message)
-			if status == True: return
-
-		curtime = int(time())
-		for treply in self.timed_replies:
-			if not curtime >= self.last_reply+self.chattiness:
-				logger.logger.warning('Timed reply cooldown left %d', self.last_reply+self.chattiness-curtime)
-				return
-			status = getattr(self, treply)(channel, user, message)
-			if status == True:
-				self.last_reply = curtime
-				return
-
 	def quit(self, *args):
 		self.disconnect('Bye Bye')
 		logger.logger.warning("Disconnected from the server!")
 		raise SystemExit
 
-	def _get_replies(self):
-		return sorted([reply for reply in dir(self) if reply.startswith('reply_')])
-
-	def _get_timed_replies(self):
-		return sorted([reply for reply in dir(self) if reply.startswith('timed_reply_')])
-
-	def _4chan_title(self, path):
-		path = path.split('/')
-		board = path[1]
-		thread = path[-1]
-		op = json.loads(urlopen('https://api.4chan.org/%s/res/%s.json' %\
-				(board, thread)).read().decode())['posts'][0]
-		comment = subject = None
-		replies, images = op['replies'], op['images']
-
-		if 'com' in op:
-			comment = BeautifulSoup(op['com']).text
-			if len(comment) > 100: comment = comment[:100]+'...'
-		if 'sub' in op: subject = op['sub']
-
-		message = 'Board: /%s/ | R: %s, I: %s | Subject: %s | Comment: %s'\
-				% (board, replies, images, str(subject), str(comment))
-
-		return message
-
-	def reply_url_title(self, channel, user, msg):
-		"""if found, resolve the title of a url in the message."""
-		if re.search(self.url_pattern, msg):
-			url = re.findall(self.url_pattern, msg)[0][0]
-
-			if url.startswith('www.'):
-				url = 'http://'+url
-
-			logger.logger.warning('Detected url %s' % str(url))
-			headers = urlopen(HeadRequest(url)).headers.dict
-			if not 'text/html' in headers['content-type']:
-				self.say(channel, '[URI] `%s` %s' % (headers['content-type'],
-					convert_bytes(headers['content-length'])))
-				return
-
-			p_url = urlparse(url)
-			if p_url.netloc == 'boards.4chan.org' and re.match(r'^/\w+/res/(\d+|\d+#p\d+)$', p_url.path):
-				logger.logger.warning('%s is a 4chan thread' % (url))
-				self.say(channel, self._4chan_title(p_url.path))
-				return
-
-			data = urlopen(url).read()
-			soup = BeautifulSoup(data)
-			if soup.title:
-				title = soup.title.string
-				self.say(channel, "[URI] %s" % title)
-				return True
-
-	def timed_reply_smiley(self, channel, user, msg):
-		smileys = re.findall(self.smiley_pattern, msg)
-		if smileys:
-			logger.logger.warning('Found %s in %s', ', '.join(smileys), msg)
-			choice = random.choice(smileys)
-			logger.logger.warning('replying with %s', choice)
-			self.tell(channel, user, choice)
-			return True
-
 	def _identify(self):
 		self.connection.privmsg('nickserv', 'identify %s' % self.password)
+
+	def _ghost(self, password):
+		self.connection.privmsg('nickserv', 'ghost %s %s' % (self.my_nick, password))
+		self.connection.nick(self.my_nick)
+		self.nick = self.my_nick[:]
+		self._identify()
 
 	def event_logger(self, event):
 		pass
@@ -319,10 +192,11 @@ class HeadRequest(Request):
 def main():
 	c = get_config('CONNECTION')
 	bot = GentooBot(c['server'],c['port'],c['channel'],c['nick'], c['password'],
-		c['reconnect'])
+			c['reconnect'])
 	logger.logger.warning('Connecting %s to %s in %s' % (c['nick'],c['channel'],
 		c['server']))
-	signal(SIGINT, bot.quit)
+	signal.signal(signal.SIGINT, bot.quit)
+
 	try:
 		bot.start()
 	except Exception as e:
